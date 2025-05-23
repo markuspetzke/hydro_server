789e191b45c6f7e6be6999469de6de134d9ca209
Markus <markuspetzke@gmail.com>
2025-05-23 21:04:58 +0200

    change to json && added sensor_id

--- api/get_ph.php
+++ api/get_ph.php
@@ -1,4 +1,20 @@
-<?php 
+<?php
+
+/**
+ * Footer
+ *
+ * Main footer file for the theme.
+ * php version 8.3.21
+ *
+ * @category   Components
+ * @package    WordPress
+ * @subpackage Theme_Name_Here
+ * @author     Your Name <yourname@example.com>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://yoursite.com
+ * @since      1.0.0
+ */
+header('Content-Type: application/json');
 $env = parse_ini_file('.env');
 
 $servername = $env["DATABASE_URL"];
@@ -8,24 +24,26 @@ $dbname = $env["DATABASE_NAME"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if($conn-> connect_error) {
-  die("Connection failed: ". $conn->connect_error);
+if ($conn-> connect_error) {
+    echo json_encode(["Connection failed" => $conn->connect_error]);
+    die();
 }
 
-$stmt = $conn->prepare("SELECT ph_value, mess_time FROM hydro
-        WHERE mess_time >= NOW() - INTERVAL 1 HOUR AND ph_value IS NOT NULL");
+$stmt = $conn->prepare(
+    "SELECT ph_value, sensor_id, mess_time FROM hydro
+     WHERE mess_time >= NOW() - INTERVAL 1 HOUR AND ph_value IS NOT NULL"
+);
 
 $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-  $output = $result->fetch_all(MYSQLI_ASSOC);
-  header('Content-Type: application/json; charset=utf-8');
-  echo json_encode($output);
+    $output = $result->fetch_all(MYSQLI_ASSOC);
+    echo json_encode($output);
 
 } else {
-    echo "Keine Einträge gefunden.";
+    echo json_encode(["error" => 'No entry found']);
+    die();
 }
 
 $insert->close();
-?>
