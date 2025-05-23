dc515a97f006dfccc4b5f53f45d225e19a949951
Markus <markuspetzke@gmail.com>
2025-05-23 17:05:49 +0200

    added stupid php doc
    so php can shut up now

--- api/add_ph.php
+++ api/add_ph.php
@@ -1,4 +1,19 @@
-<?php 
+<?php
+
+/**
+ * Footer
+ *
+ * Main footer file for the theme.
+ * php version 8.3.21
+ *
+ * @category   Components
+ * @package    WordPress
+ * @subpackage Theme_Name_Here
+ * @author     Your Name <yourname@example.com>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://yoursite.com
+ * @since      1.0.0
+ */
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $env = parse_ini_file('.env');
@@ -10,13 +25,16 @@ $dbname = $env["DATABASE_NAME"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if($conn-> connect_error) {
-  die("Connection failed: ". $conn->connect_error);
+if ($conn-> connect_error) {
+    die("Connection failed: ". $conn->connect_error);
 }
 
 echo "Connected";
 
 $ph_value = $_POST['ph_value'];
+if (empty($ph_value)) {
+    die("ph_value is empty");
+}
 $insert = $conn->prepare("INSERT INTO hydro (ph_value) VALUES (?)");
 $insert->bind_param("s", $ph_value);
 $insert->execute();
@@ -28,4 +46,3 @@ if ($insert->affected_rows > 0) {
 }
 
 $insert->close();
-?>
